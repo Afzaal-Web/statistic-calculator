dc368beb8eaf7ff94a77b2018bcd807641daf4a7
Afzaal Web <afzaalwebsolutions@gmail.com>
2025-06-22 15:57:30 +0500

    Variance method added

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ const getMode = (array) => {
   }
   console.log('keys: ', Object.keys(counts));
   console.log('Values: ', Object.values(counts));
-  const highest = Object.keys(counts).sort((a,b) => counts[b] - counts[a])[0];
+  const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0];
   const mode = Object.keys(counts).filter((el) => counts[el] === counts[highest]);
   console.log(mode)
   return mode.join(', ');
@@ -66,6 +66,27 @@ const getRange = (array) => {
   return highest - lowest;
 }
 
+const getVariance = (array) => {
+  /*  const mean = getMean(array);
+   const differences = array.map(
+     el => el - mean
+   );
+   const squaredDifferences = differences.map(
+     el => el ** 2
+   );
+   const sumSquaredDifferences = squaredDifferences.reduce(
+     (acc, el) => acc + el, 0
+   ); */
+
+  const mean = getMean(array);
+  const variance = array.reduce((acc, el) => {
+    const difference = el - mean;
+    const squared = difference ** 2;
+    return acc + squared;
+  }, 0) / array.length;
+  return variance;
+}
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
@@ -74,11 +95,13 @@ const calculate = () => {
 
   const mean = getMean(numbers);
   const median = getMedian(numbers);
- const mode = getMode(numbers);
- const range = getRange(numbers);
+  const mode = getMode(numbers);
+  const range = getRange(numbers);
+  const variance = getVariance(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
   document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
 }
